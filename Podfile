12a929915e3c59733ded8c31787f763cb3721a50
Max Mashkov <mashkovmax@gmail.com>
2013-12-04 16:21:50 +0200

    initial commit

---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '6.0'
+
+workspace 'TouchAtlas'
+
+pod 'AppleGuice', '1.0.0'
+pod 'MAObjCRuntime', '0.0.1'
---
+++ TouchAtlas/Classes/DXTAAppDelegate.h
@@ -0,0 +1,16 @@
+//
+// Created by zen on 6/2/13.
+// Copyright (c) 2013 111min. All rights reserved.
+//
+// To change the template use AppCode | Preferences | File Templates.
+//
+
+#import <MAObjCRuntime/MARTNSObject.h>
+#import <AppleGuice/AppleGuice.h>
+#import <Foundation/Foundation.h>
+
+@interface DXTAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
